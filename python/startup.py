2ece95a5aad26e463505973e8d9f4e7f60c3b164
Ethan Wu <ethanwu@cs.ucsb.edu>
2022-01-05 15:09:58 -0800

    Fix python history

--- python/startup.py
+++ python/startup.py
@@ -1,16 +1,20 @@
-# https://unix.stackexchange.com/a/121390
+# based on https://unix.stackexchange.com/a/121390
+
 import atexit
 import os
+from pathlib import Path
 import readline
 
 cache_dir = os.environ['XDG_CACHE_HOME']
-if cache_dir is None:
-    cache_dir = os.path.expanduser('-')
-histfile = os.path.join(cache_dir, 'python', 'python_history')
+if cache_dir:
+    cache_dir = Path(cache_dir, 'python')
+else:
+    cache_dir = Path('~', '.python')
+cache_dir.mkdir(parents=True, exist_ok=True)
+histfile = cache_dir / 'history'
 
 try:
     readline.read_history_file(histfile)
-    readline.set_history_length(0)
 except FileNotFoundError:
     pass
 
