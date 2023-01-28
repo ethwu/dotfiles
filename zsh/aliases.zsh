ae7ed463c99bb5c833410b2f5921dad8b2cbb74d
Ethan Wu <ethanwu@cs.ucsb.edu>
2023-01-28 12:39:50 -0800

    Update prompt and aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,3 +15,14 @@ if command -v exa &> /dev/null ; then
     alias desc='ls -dlbghH@ --git'
 fi
 
+if command -v ranger &> /dev/null ; then
+    alias R=ranger
+fi
+
+if command -v nvim &> /dev/null ; then
+    alias vim=nvim
+fi
+
+if command -v just &> /dev/null ; then
+    alias j=just
+fi
